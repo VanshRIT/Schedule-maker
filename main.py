de09d487c375e93d91e360dcdbcef89c6c56d50a
VanshRIT <vpp4868@rit.edu>
2023-04-30 23:50:12 +0400

    basic working outline

--- main.py
+++ main.py
@@ -1,49 +1,44 @@
 import csv
 
-def get_courses(num_courses):
-    courses = []
-    for i in range(num_courses):
-        course = input(f"Enter course {i+1}: ")
-        courses.append(course)
-    return courses
+# Get input from user
+num_courses = int(input("Enter the number of courses: "))
+courses = []
+for i in range(num_courses):
+    subject = input("Enter the subject of course {}: ".format(i+1))
+    cat_num = input("Enter the catalog number of course {}: ".format(i+1))
+    courses.append((subject, cat_num))
 
-def parse_schedule(file_name):
-    with open(file_name, 'r') as f:
-        reader = csv.reader(f)
-        schedule = {}
-        next(reader) # skip the header row
-        for row in reader:
-            course_key = row[1] + ' ' + row[2]
-            section = row[3]
-            time = f"{row[9]} {row[10]}-{row[11]}"
-            if course_key not in schedule:
-                schedule[course_key] = {}
-            if section not in schedule[course_key]:
-                schedule[course_key][section] = []
-            schedule[course_key][section].append(time)
-    return schedule
+# Parse CSV file and create list of classes
+classes = []
+with open('class_schedule.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)  # skip header row
+    for row in reader:
+        if (row[2], row[3]) in courses:
+            course = row[2] + ' ' + row[3]
+            section = row[4]
+            days = row[10]
+            time_start = row[11]
+            time_end = row[12]
+            if days and time_start and time_end:
+                time = (days, time_start, time_end)
+                classes.append((course, section, time))
 
+# Sort list of classes by time
+classes.sort(key=lambda x: x[2])
 
-def find_best_schedule(courses, schedule):
-    sections = {}
-    for course in courses:
-        sections[course] = []
-        for section in schedule[course]:
-            if not any(time in sections[course] for time in schedule[course][section]):
-                sections[course].append(section)
-    return sections
+# Create list of sections with no conflicts
+sections = []
+for c in classes:
+    conflict = False
+    for s in sections:
+        if c[2][0] == s[2][0] and c[2][1] < s[2][2] and c[2][2] > s[2][1]:
+            conflict = True
+            break
+    if not conflict:
+        sections.append(c)
 
-def print_schedule(sections):
-    print("Class schedule:")
-    for course in sections:
-        print(course)
-        for section in sections[course]:
-            print(f"Section {section}")
-        print()
-
-num_courses = int(input("Enter number of courses: "))
-courses = get_courses(num_courses)
-schedule = parse_schedule('class_schedule.csv')
-#print(schedule)
-sections = find_best_schedule(courses, schedule)
-print_schedule(sections)
+# Output sections
+print("Best possible schedule:")
+for s in sections:
+    print(s[0], s[1], s[2][0], s[2][1], s[2][2])
