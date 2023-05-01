5b3a573ad99173d4f6ff46a2e5506b68ee6bd70e
VanshRIT <vpp4868@rit.edu>
2023-05-01 18:27:47 +0400

    added flask dependency

--- main.py
+++ main.py
@@ -1,64 +1,61 @@
+from flask import Flask, render_template, request
+
 import csv
-import itertools
-from datetime import datetime, time
 
-# Get input from user
-num_courses = int(input("Enter the number of courses: "))
-courses = []
-for i in range(num_courses):
-    subject = input("Enter the subject of course {}: ".format(i + 1)).upper()
-    cat_num = input("Enter the catalog number of course {}: ".format(i + 1))
-    courses.append((subject, cat_num))
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'test'
 
-# Parse CSV file and create list of classes
-classes = {}
 
-for course in courses:
-    classes[course] = []
 
-with open('class_schedule.csv', 'r') as csvfile:
-    reader = csv.reader(csvfile)
-    next(reader)  # skip header row
 
-    for row in reader:
-        if (row[2], row[3]) in courses:
-            course = row[2] + ' ' + row[3]
-            section = row[4]
-            days = row[10]
 
-            try:
-                time_start = datetime.strptime(row[11], '%I:%M %p').time().strftime('%H:%M')
-                time_end = datetime.strptime(row[12], '%I:%M %p').time().strftime('%H:%M')
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/schedule', methods=['POST'])
+def schedule():
+    course_count = int(request.form['course-count'])
+    courses = []
+    for i in range(1, course_count + 1):
+        subject = request.form.get(f'course{i}-subject')
+        catalog_num = request.form.get(f'course{i}-catalog-num')
+        if subject and catalog_num:
+            courses.append((subject, catalog_num))
+    # Parse CSV file and create list of classes
+    classes = {}
+    with open('class_schedule.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)  # skip header row
+        for row in reader:
+            if (row[2], row[3]) in courses:
+                course_key = row[2] + ' ' + row[3]
+                section = row[4]
+                days = row[10]
+                time_start = row[11]
+                time_end = row[12]
+                if days and time_start and time_end:
+                    time = (days, time_start, time_end)
+                    if course_key in classes:
+                        classes[course_key].append((section, time))
+                    else:
+                        classes[course_key] = [(section, time)]
 
-            except:
-                continue
+    # Create list of sections with no conflicts
+    sections = []
+    for course_key, sections_list in classes.items():
+        for section1 in sections_list:
+            conflict = False
+            for section2 in sections:
+                if section1[1][0] == section2[1][0] and section1[1][1] < section2[1][2] and section1[1][2] > section2[1][1]:
+                    conflict = True
+                    break
+            if not conflict:
+                sections.append((course_key, section1))
 
-            if days and time_start and time_end:
-                time = (days, time_start, time_end)
-                classes[(row[2], row[3])].append((course, section, time))
+    # Render template with list of sections
+    return render_template('schedule.html', sections=sections)
 
-combos = itertools.product(*classes.values())
-no_clash = []
 
-for combo in combos:
-    clash = False
-    for i, course1 in enumerate(combo):
-        for j, course2 in enumerate(combo):
-            if i != j and (course1[2][0] in course2[2][0] or course1[2][0] in course2[2][0]):
-                if (course1[2][1] <= course2[2][1] < course1[2][2]) or (
-                        course2[2][1] <= course1[2][1] < course2[2][2]) or \
-                        (course1[2][1] <= course2[2][2] < course1[2][2]) or (
-                        course2[2][1] <= course1[2][2] < course2[2][2]) or \
-                        (course2[2][1] <= course1[2][1] and course1[2][2] <= course2[2][2]) or \
-                        (course1[2][1] <= course2[2][1] and course2[2][2] <= course1[2][2]):
-                    clash = True
 
-    if not clash:
-        no_clash.append(combo)
-
-# Output section
-print("Best possible schedule:")
-for combo in no_clash:
-    for s in combo:
-        print(s[0], s[1], s[2][0], s[2][1], s[2][2])
-    print("\n\n")
+if __name__ == "__main__":
+    app.run(debug=True)
